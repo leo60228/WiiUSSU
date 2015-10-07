4c05547cc05d1ee3a594c84b204a7a1b91d2d3fa
snuggles08 <iakornfeld@gmail.com>
2015-10-07 10:16:10 -0400

    Added the currently selected screen to the UI

---
+++ public/script.js
@@ -0,0 +1,17 @@
+var timeout = setInterval(function() {
+  if ($('#file-select')[0].files && $('#file-select')[0].files[0]) {
+    var file = $('#file-select')[0].files[0];
+
+    if (window.filename !== file.name) {
+      window.filename = file.name;
+      
+      var selected = document.getElementById('selected');
+
+      selected.innerHTML = file.name.replace(/^WiiU_.*?_(TV|GamePad)_.*$/gm, '$1');
+    }
+  } else if ((!($('#file-select')[0].files) || !($('#file-select')[0].files[0]) && window.filename)) {
+    var selected = document.getElementById('selected');
+
+    selected.innerHTML = 'None';
+  }
+}, 1000);
