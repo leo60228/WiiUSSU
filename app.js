88de7c0d6c04fb5b3b0b4a73f39b805e46e0b7ba
snuggles08 <iakornfeld@gmail.com>
2015-12-29 17:08:15 -0500

    Added API

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-var express = require('express');
-var app = express();
-var multer = require('multer');
-var fs = require('fs-extra');
-var mime = require('mime-types');
+function run(dir) {
+  dir = typeof dir === 'undefined' ? (__dirname + '/screenshots/') : dir;
+  var express = require('express');
+  var app = express();
+  var multer = require('multer');
+  var fs = require('fs-extra');
+  var mime = require('mime-types');
 
-fs.ensureDirSync(__dirname + '/screenshots/');
+  fs.ensureDirSync(dir);
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname + '/screenshots/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + '.' + (mime.extension(file.mimetype) || 'bin'));
-  }
-});
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, dir);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.' + (mime.extension(file.mimetype) || 'bin'));
+    }
+  });
 
-var upload = multer({storage: storage})
+  var upload = multer({storage: storage})
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
-app.post('/upload', upload.single('screenshot'), function(req, res, next) {
-  res.redirect('/');
-});
+  app.post('/upload', upload.single('screenshot'), function(req, res, next) {
+    res.redirect('/');
+  });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('App listening at http://%s:%s', host, port);
-});
+    console.log('App listening at http://%s:%s', host, port);
+  });
+}
+
+module.exports = run;
+
+if (require.main === module) {
+  run();
+}
